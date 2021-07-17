2ca67c705c3ac6abf65be1d261a023ce9fdf95b0
MasaNom22 <masakatsunomura26@gmail.com>
2021-07-17 23:41:18 +0900

    レイアウト修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
         @yield('styles')
 
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!--<link href="{{ asset('css/calendar.css') }}" rel="stylesheet">-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
@@ -21,7 +20,7 @@
     </head>
 
     <body>
-        <div id="app">
+        <div id="wrapper">
             @include('navbar')
 
             @yield('content')
@@ -29,7 +28,6 @@
             @yield('scripts')
         </div>
         
-            
             <!-- JQuery -->
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
             <!-- Bootstrap tooltips -->
