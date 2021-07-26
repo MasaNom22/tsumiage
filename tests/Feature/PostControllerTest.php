f1c0c476ca99dd91184f50eebc3555b7573d19ce
MasaNom22 <masakatsunomura26@gmail.com>
2021-07-26 23:36:18 +0900

    テストコード追加

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,5 +22,31 @@ class PostControllerTest extends TestCase
         $response->assertStatus(200)
             ->assertViewIs('posts.index')
             ->assertSee('積み上げアプリ');
+    }
+
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('posts.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testAuthCreate()
+    {
+        //use App\User;が必要
+        $user = factory(User::class)->create();
+        //ログインしたユーザーでアクセス
+        $response = $this->actingAs($user)->get(route('posts.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('posts.create')
+            ->assertSee('投稿画面');
+    }
+
+    public function testGuestStore()
+    {
+        $response = $this->post(route('posts.store'));
+
+        $response->assertRedirect(route('login'));
     }
 }
